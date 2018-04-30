d7c9bbfc254bd0ca4fa778f714d222a2a2944e75
Francois Wouts <f@zenc.io>
2018-05-01 09:26:57 +1000

    Fix broken storybook

--- example/src/react/.storybook/config.js
+++ example/src/react/.storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from "@storybook/react";
 
 function loadStories() {
-  require("__src__react__component_story/component.story");
+  require("__example__src__react__component_story/component.story");
 }
 
 configure(loadStories, module);
