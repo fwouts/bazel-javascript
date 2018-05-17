97d67b6c20ca28a7f26d226b6f445b9df3b7b289
Francois Wouts <f@zenc.io>
2018-05-18 09:17:45 +1000

    Add file-loader to support referring to static assets such as images

--- internal/js_bundle/compile.js
+++ internal/js_bundle/compile.js
@@ -101,6 +101,14 @@ webpack(
                 "postcss-loader",
                 "sass-loader"
               ]
+            },
+            {
+              // Exclude `js` files to keep "css" loader working as it injects
+              // its runtime that would otherwise be processed through "file" loader.
+              // Also exclude `html` and `json` extensions so they get processed
+              // by webpacks internal loaders.
+              exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+              loader: require.resolve("file-loader")
             }
           ]
         }
