22714dc522e29ae2698bfeae5e163548fcee28e0
Francois Wouts <f@zenc.io>
2018-07-14 11:50:44 +1000

    Use babel-core instead of babel-cli

--- internal/js_library/compile.js
+++ internal/js_library/compile.js
@@ -1,6 +1,6 @@
-const child_process = require("child_process");
 const fs = require("fs-extra");
 const path = require("path");
+const babel = require("babel-core");
 
 const [nodePath, scriptPath, fullSrcDir, destinationDir] = process.argv;
 
@@ -18,11 +18,34 @@ fs.copySync(fullSrcDir, destinationDir, {
 });
 
 // Compile with Babel.
-child_process.execSync(
-  `${
-    process.env.NODE_PATH
-  }/.bin/babel ${fullSrcDir} --presets env,stage-2,react --plugins transform-decorators-legacy,transform-es2015-modules-commonjs --ignore node_modules --out-dir ${destinationDir}`,
-  {
-    stdio: "inherit"
+transformDir(".");
+
+function transformDir(dirRelativePath) {
+  for (const fileName of fs.readdirSync(
+    path.join(fullSrcDir, dirRelativePath)
+  )) {
+    const srcFilePath = path.join(fullSrcDir, dirRelativePath, fileName);
+    if (fs.lstatSync(srcFilePath).isDirectory()) {
+      transformDir(path.join(dirRelativePath, fileName));
+    } else if (
+      fileName.endsWith(".es6") ||
+      fileName.endsWith(".js") ||
+      fileName.endsWith(".jsx")
+    ) {
+      const transformed = babel.transformFileSync(srcFilePath, {
+        plugins: [
+          "transform-decorators-legacy",
+          "transform-es2015-modules-commonjs"
+        ],
+        presets: ["env", "stage-2", "react"],
+        ignore: "node_modules"
+      });
+      if (!transformed.code) {
+        throw new Error(`Could not compile ${srcFilePath}.`);
+      }
+      const destFilePath = path.join(destinationDir, dirRelativePath, fileName);
+      fs.ensureDirSync(path.dirname(destFilePath));
+      fs.writeFileSync(destFilePath, transformed.code, "utf8");
+    }
   }
-);
+}
