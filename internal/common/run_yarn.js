5f67facbc25340d24bf72fba9f1afb50eb7284e8
Francois Wouts <f@zenc.io>
2018-07-14 13:35:59 +1000

    Create the Yarn cache if it doesn't exist yet

--- internal/common/run_yarn.js
+++ internal/common/run_yarn.js
@@ -1,6 +1,12 @@
 const child_process = require("child_process");
+const fs = require("fs");
+
+const CACHE_PATH = "/tmp/bazel_node_modules_cache";
 
 function runYarn(cwd, command = "") {
+  if (!fs.existsSync(CACHE_PATH)) {
+    fs.mkdirSync(CACHE_PATH);
+  }
   child_process.execSync(yarnShellCommand(cwd, command), {
     stdio: "inherit"
   });
@@ -9,7 +15,7 @@ function runYarn(cwd, command = "") {
 function yarnShellCommand(cwd, command = "") {
   // Don't use the shared cache or touch the lockfile.
   // See https://github.com/yarnpkg/yarn/issues/986.
-  return `yarn --cwd ${cwd} --ignore-scripts --cache-folder /tmp/bazel_node_modules_cache --frozen-lockfile ${command}`;
+  return `yarn --cwd ${cwd} --ignore-scripts --cache-folder ${CACHE_PATH} --frozen-lockfile ${command}`;
 }
 
 module.exports = {
