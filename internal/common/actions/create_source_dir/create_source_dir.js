96d1baa11455114862a5a9388351fb03b7c4febd
Rich Adams <richard.adams@intelight-its.com>
2018-10-11 06:26:15 -0700

    Removing some cruft

--- internal/common/actions/create_source_dir/create_source_dir.js
+++ internal/common/actions/create_source_dir/create_source_dir.js
@@ -1,10 +1,6 @@
 const fs = require("fs-extra");
 const path = require("path");
-const {
-  BazelAction,
-  safeSymlink,
-  ensureArray
-} = require("../run_js/BazelAction");
+const { BazelAction, ensureArray } = require("../run_js/BazelAction");
 
 /**
  * Action for creating a directory with the passed in files for symlinking and
@@ -91,83 +87,16 @@ BazelAction(
       }
     };
 
-    console.log(`Current Target: ${current_target}`);
-    console.log(`Target Source Path: ${from}`);
-    console.log(`Copying into: ${into}`);
-    console.log(`Running in [${process.cwd()}]`);
-    console.log(`Environment is: ${JSON.stringify(process.env, null, 2)}`);
-    console.log(`Will create [${into}] for populating`);
+    // console.log(`Current Target: ${current_target}`);
+    // console.log(`Target Source Path: ${from}`);
+    // console.log(`Copying into: ${into}`);
+    // console.log(`Running in [${process.cwd()}]`);
+    // console.log(`Environment is: ${JSON.stringify(process.env, null, 2)}`);
+    // console.log(`Will create [${into}] for populating`);
 
     for (const source of sources) {
       await populateFiles(source);
     }
-
-    // const internalDeps = joinedInternalDeps.split("|");
-    // const srcs = joinedSrcs.split("|");
-
-    //     fs.mkdirSync(into);
-    //     safeSymlink(
-    //       path.join(installedNpmPackagesDir, "node_modules"),
-    //       path.join(destinationDir, "node_modules")
-    //     );
-
-    //     // Copy every internal dependency into the appropriate location.
-    //     for (const internalDep of internalDeps) {
-    //       if (!internalDep) {
-    //         continue;
-    //       }
-    //       const [joinedSrcs, compiledDir] = internalDep.split(":");
-    //       const srcs = joinedSrcs.split(";");
-    //       for (const src of srcs) {
-    //         if (!src) {
-    //           continue;
-    //         }
-    //         safeSymlink(
-    //           path.join(compiledDir, src),
-    //           path.join(destinationDir, src)
-    //         );
-    //       }
-    //     }
-
-    //     // Extract compiler options from tsconfig.json, overriding anything other
-    //     // than compiler options.
-    //     const originalTsConfig = JSON.parse(fs.readFileSync(tsconfigPath, "utf8"));
-
-    //     // Copy source code and update import statements in this target's sources.
-    //     for (const src of srcs) {
-    //       if (!src) {
-    //         continue;
-    //       }
-    //       if (!fs.existsSync(src)) {
-    //         console.error(`
-    // Missing file ${src} required by ${targetLabel}.
-    // `);
-    //         process.exit(1);
-    //       }
-    //       const destinationFilePath = path.join(destinationDir, src);
-    //       fs.ensureDirSync(path.dirname(destinationFilePath));
-    //       safeSymlink(src, destinationFilePath);
-    //     }
-
-    //     const compilerOptions = {};
-    //     Object.assign(compilerOptions, originalTsConfig.compilerOptions || {});
-    //     Object.assign(compilerOptions, {
-    //       moduleResolution: "node",
-    //       declaration: true,
-    //       rootDir: "."
-    //     });
-    //     fs.writeFileSync(
-    //       path.join(destinationDir, "tsconfig.json"),
-    //       JSON.stringify(
-    //         {
-    //           compilerOptions,
-    //           files: srcs.filter(src => src.endsWith(".ts") || src.endsWith(".tsx"))
-    //         },
-    //         null,
-    //         2
-    //       ),
-    //       "utf8"
-    //     );
   }
 );
 
