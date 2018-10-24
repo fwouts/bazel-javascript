872b6938292ad40abf7e360596cecb59ac12ff84
Francois Wouts <f@zenc.io>
2018-10-25 08:24:11 +1100

    Fix prettier issues

--- examples/react-storybook-typescript/src/component.story.tsx
+++ examples/react-storybook-typescript/src/component.story.tsx
@@ -1,14 +1,8 @@
-import * as React from 'react';
-import { storiesOf } from '@storybook/react';
-import { Component1 } from './component1';
-import { Component2 } from './component2';
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import { Component1 } from "./component1";
+import { Component2 } from "./component2";
 
-storiesOf('Component 1', module)
-  .add('testing', () => (
-    <Component1 />
-  ))
+storiesOf("Component 1", module).add("testing", () => <Component1 />);
 
-storiesOf('Component 2', module)
-  .add('testing', () => (
-    <Component2 />
-  ))
+storiesOf("Component 2", module).add("testing", () => <Component2 />);
