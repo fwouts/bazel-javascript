83284cc48dab1460a52371cd01dc112ff83a891b
Sebastian Choren <sebastianchoren@gmail.com>
2019-02-13 16:32:59 -0300

    Update js_test/js_script to use bazel-managed yarn

--- internal/common/run_yarn.js
+++ internal/common/run_yarn.js
@@ -9,11 +9,11 @@ function runYarn(cwd, command = "") {
   });
 }
 
-function yarnShellCommand(cwd, command = "") {
+function yarnShellCommand(cwd, command = "", yarn = "yarn") {
   // Don't use the shared cache or touch the lockfile.
   // See https://github.com/yarnpkg/yarn/issues/986.
   const cachePath = path.resolve(cwd, CACHE_PATH);
-  return `yarn --cwd ${cwd} --cache-folder ${cachePath} --frozen-lockfile ${command}`;
+  return `${yarn} --cwd ${cwd} --cache-folder ${cachePath} --frozen-lockfile ${command}`;
 }
 
 module.exports = {
