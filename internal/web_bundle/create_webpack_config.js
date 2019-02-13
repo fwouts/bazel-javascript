b4ca176b18b39b786912cc84fd8211a74aa014a1
Sebastian Choren <sebastianchoren@gmail.com>
2019-02-13 12:59:37 -0300

    Fix js formatting

--- internal/web_bundle/create_webpack_config.js
+++ internal/web_bundle/create_webpack_config.js
@@ -66,20 +66,20 @@ module.exports = (
       path: path.resolve(outputBundleDir),
       publicPath: "${publicPath}",
       ${
-  optionalLibrary
-    ? `library: "${libraryName}",
+        optionalLibrary
+          ? `library: "${libraryName}",
       libraryTarget: "${libraryTarget}",
       `
-    : ""
-  }
+          : ""
+      }
     },
     mode: "${mode}",
     bail: ${mode === "production" ? "true" : "false"},
     target: "web",
     optimization: {
       ${
-  mode === "production"
-    ? `minimizer: [
+        mode === "production"
+          ? `minimizer: [
         new UglifyJsPlugin({
           uglifyOptions: {
             parse: {
@@ -105,17 +105,17 @@ module.exports = (
         }),
         new OptimizeCSSAssetsPlugin(),
       ],`
-    : ""
-  }
+          : ""
+      }
       ${
-  splitChunks
-    ? `splitChunks: {
+        splitChunks
+          ? `splitChunks: {
         chunks: 'all',
         name: 'vendors',
       },
       runtimeChunk: true,`
-    : ""
-  }
+          : ""
+      }
     },
     module: {
       rules: [
@@ -125,10 +125,10 @@ module.exports = (
               test: /\\.module\\.css$/,
               use: [
                 ${
-  mode === "production"
-    ? "MiniCssExtractPlugin.loader"
-    : '"style-loader"'
-  },
+                  mode === "production"
+                    ? "MiniCssExtractPlugin.loader"
+                    : '"style-loader"'
+                },
                 {
                   loader: "css-loader",
                   options: {
@@ -142,10 +142,10 @@ module.exports = (
               test: /\\.module\\.scss$/,
               use: [
                 ${
-  mode === "production"
-    ? "MiniCssExtractPlugin.loader"
-    : '"style-loader"'
-  },
+                  mode === "production"
+                    ? "MiniCssExtractPlugin.loader"
+                    : '"style-loader"'
+                },
                 {
                   loader: "css-loader",
                   options: {
@@ -161,10 +161,10 @@ module.exports = (
               test: /\\.css$/,
               use: [
                 ${
-  mode === "production"
-    ? "MiniCssExtractPlugin.loader"
-    : '"style-loader"'
-  },
+                  mode === "production"
+                    ? "MiniCssExtractPlugin.loader"
+                    : '"style-loader"'
+                },
                 "css-loader"
               ]
             },
@@ -172,10 +172,10 @@ module.exports = (
               test: /\\.scss$/,
               use: [
                 ${
-  mode === "production"
-    ? "MiniCssExtractPlugin.loader"
-    : '"style-loader"'
-  },
+                  mode === "production"
+                    ? "MiniCssExtractPlugin.loader"
+                    : '"style-loader"'
+                },
                 {
                   loader: "css-loader",
                   options: {
@@ -230,8 +230,8 @@ module.exports = (
           template: htmlTemplatePath,
           inject: true,
           ${
-  mode === "production"
-    ? `minify: {
+            mode === "production"
+              ? `minify: {
             removeComments: true,
             collapseWhitespace: true,
             removeRedundantAttributes: true,
@@ -243,8 +243,8 @@ module.exports = (
             minifyCSS: true,
             minifyURLs: true,
           }`
-    : ""
-  }
+              : ""
+          }
         })]
         : []
       ),
@@ -253,21 +253,21 @@ module.exports = (
         NODE_ENV: "${mode}",
       }),
       ${
-  mode === "production"
-    ? `new MiniCssExtractPlugin({
+        mode === "production"
+          ? `new MiniCssExtractPlugin({
         filename: '[name].[contenthash].css',
         chunkFilename: '[name].[contenthash].chunk.css',
       }),`
-    : ""
-  }
+          : ""
+      }
       ${
-  // Chunk splitting is enabled by default.
-  splitChunks
-    ? `new webpack.optimize.LimitChunkCountPlugin({
+        // Chunk splitting is enabled by default.
+        splitChunks
+          ? `new webpack.optimize.LimitChunkCountPlugin({
         maxChunks: 1
       }),`
-    : ""
-  }
+          : ""
+      }
     ]
   };
 }
